1b08db709d3767088b1f007f556fbbb4c98e8fbf
Arun_Kumar_Verma <arunkrverma85@gmail.com>
2020-07-24 14:20:01 +0530

    formatting

--- src/credit_card.js
+++ src/credit_card.js
@@ -17,7 +17,7 @@ class CreditCardInput extends React.Component {
     }
 
     changehandler(e) {
-        
+
         this.setState({
             [e.target.name]: e.target.value
         })
@@ -35,7 +35,7 @@ class CreditCardInput extends React.Component {
 
     pasteCreditNumber(e) {
         var cardNumberPastedValue = (e.clipboardData.getData('text/plain'));
-        var {cardNumber} = this.state;
+        var { cardNumber } = this.state;
 
         cardNumber = cardNumberPastedValue.match(/.{1,4}/g);
 
@@ -52,12 +52,48 @@ class CreditCardInput extends React.Component {
         return (
             <div>
                 <label>
-                    Card Number* 
+                    Card Number*
                 </label>
-                <input type="text" id='ip0' name="inp0" size="16" value={this.state.inp0 ? this.state.inp0 : ''} onChange={this.changehandler.bind(this)} onPaste={this.pasteCreditNumber.bind(this)} onKeyUp={this.changeFocus.bind(this)} maxLength="4" />
-                <input type="text" id='ip1' name="inp1" size="16" value={this.state.inp1 ? this.state.inp1 : ''} onChange={this.changehandler.bind(this)} onPaste={this.pasteCreditNumber.bind(this)} onKeyUp={this.changeFocus.bind(this)} maxLength="4" />
-                <input type="text" id='ip2' name="inp2" size="16" value={this.state.inp2 ? this.state.inp2 : ''} onChange={this.changehandler.bind(this)} onPaste={this.pasteCreditNumber.bind(this)} onKeyUp={this.changeFocus.bind(this)} maxLength="4" />
-                <input type="text" id='ip3' name="inp3" size="16" value={this.state.inp3 ? this.state.inp3 : ''} onChange={this.changehandler.bind(this)} onPaste={this.pasteCreditNumber.bind(this)} onKeyUp={this.changeFocus.bind(this)} maxLength="4" />
+                <input
+                    type="text"
+                    id='ip0'
+                    name="inp0"
+                    size="16"
+                    value={this.state.inp0 ? this.state.inp0 : ''}
+                    onChange={this.changehandler.bind(this)}
+                    onPaste={this.pasteCreditNumber.bind(this)}
+                    onKeyUp={this.changeFocus.bind(this)}
+                    maxLength="4" />
+                <input
+                    type="text"
+                    id='ip1'
+                    name="inp1"
+                    size="16"
+                    value={this.state.inp1 ? this.state.inp1 : ''}
+                    onChange={this.changehandler.bind(this)}
+                    onPaste={this.pasteCreditNumber.bind(this)}
+                    onKeyUp={this.changeFocus.bind(this)}
+                    maxLength="4" />
+                <input
+                    type="text"
+                    id='ip2'
+                    name="inp2"
+                    size="16"
+                    value={this.state.inp2 ? this.state.inp2 : ''}
+                    onChange={this.changehandler.bind(this)}
+                    onPaste={this.pasteCreditNumber.bind(this)}
+                    onKeyUp={this.changeFocus.bind(this)}
+                    maxLength="4" />
+                <input
+                    type="text"
+                    id='ip3'
+                    name="inp3"
+                    size="16"
+                    value={this.state.inp3 ? this.state.inp3 : ''}
+                    onChange={this.changehandler.bind(this)}
+                    onPaste={this.pasteCreditNumber.bind(this)}
+                    onKeyUp={this.changeFocus.bind(this)}
+                    maxLength="4" />
             </div>
         )
     }
