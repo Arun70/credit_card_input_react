7885b62fd7cca1a681a57b89497e45df3e002d4e
Arun_Kumar_Verma <arunkrverma85@gmail.com>
2020-07-24 13:31:15 +0530

    created credit_card_input field for required conditions in react

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+import CreditCardInput from './credit_card';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <CreditCardInput />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/credit_card.js
@@ -0,0 +1,66 @@
+import React from 'react';
+
+class CreditCardInput extends React.Component {
+
+    constructor(props) {
+        super();
+        this.state = {
+            cardNumber: [],
+            inp0: '',
+            inp1: '',
+            inp2: '',
+            inp3: ''
+        };
+        this.creditCardNumber = this.creditCardNumber.bind(this);
+        this.pasteCreditNumber = this.pasteCreditNumber.bind(this);
+        this.changehandler = this.changehandler.bind(this);
+    }
+
+    changehandler(e) {
+        
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    creditCardNumber(e) {
+        var inpId = e.target.attributes['id'].value;
+        if (e.target.value.length >= parseInt(e.target.attributes["maxLength"].value)) {
+            document.getElementById(inpId).nextElementSibling.focus();
+        }
+        else if (e.target.value.length === 0) {
+            document.getElementById(inpId).previousElementSibling.focus();
+        }
+    }
+
+    pasteCreditNumber(e) {
+        var cardNumberPastedValue = (e.clipboardData.getData('text/plain'));
+        var {cardNumber} = this.state;
+
+        cardNumber = cardNumberPastedValue.match(/.{1,4}/g);
+
+        document.getElementById('ip3').focus();
+        this.setState({
+            inp0: cardNumber[0],
+            inp1: cardNumber[1],
+            inp2: cardNumber[2],
+            inp3: cardNumber[3]
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <label>
+                    Card Number* 
+                </label>
+                <input type="text" id='ip0' name="inp0" size="16" value={this.state.inp0 ? this.state.inp0 : ''} onChange={this.changehandler.bind(this)} onPaste={this.pasteCreditNumber.bind(this)} onKeyUp={this.creditCardNumber.bind(this)} maxLength="4" />
+                <input type="text" id='ip1' name="inp1" size="16" value={this.state.inp1 ? this.state.inp1 : ''} onChange={this.changehandler.bind(this)} onPaste={this.pasteCreditNumber.bind(this)} onKeyUp={this.creditCardNumber.bind(this)} maxLength="4" />
+                <input type="text" id='ip2' name="inp2" size="16" value={this.state.inp2 ? this.state.inp2 : ''} onChange={this.changehandler.bind(this)} onPaste={this.pasteCreditNumber.bind(this)} onKeyUp={this.creditCardNumber.bind(this)} maxLength="4" />
+                <input type="text" id='ip3' name="inp3" size="16" value={this.state.inp3 ? this.state.inp3 : ''} onChange={this.changehandler.bind(this)} onPaste={this.pasteCreditNumber.bind(this)} onKeyUp={this.creditCardNumber.bind(this)} maxLength="4" />
+            </div>
+        )
+    }
+}
+
+export default CreditCardInput;
